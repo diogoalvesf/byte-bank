99c4f932ffacf478003a36ee557c64d636017703
Diogo Alves <diogohenriquealves29@gmail.com>
2022-05-27 23:05:45 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:bytebank/screens/transferencia/lista.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(BytebankApp());
+
+class BytebankApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: ListaTransferencias(),
+    );
+  }
+}
